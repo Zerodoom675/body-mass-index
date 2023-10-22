e047c52fa1356b90bbe0543e8861b3230cf9e7e7
Zerodoom <zerodoom675@mail.ru>
2023-10-22 19:27:29 +0300

    second task

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        return (int) (weight/Math.pow(height, 2));
+        return (int) (weight / Math.pow(height, 2));
 
     }
 }
